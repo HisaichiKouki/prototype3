7e392f718a1b33c86efbb9648e32f201aae05adb
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2023-10-24 15:45:56 +0900

    音を鳴らすクラスの実装

---
+++ Source/PlaySoundEffect.cpp
@@ -0,0 +1,24 @@
+﻿#include "././Header/PlaySoundEffect.h"
+
+std::vector<int>Original::handles;
+std::vector<const char*>Original::handleName;
+
+void Original::LoadAudio(const char _path[], const char _handleName[])
+{
+	Original::handles.push_back(Novice::LoadAudio(_path));
+	Original::handleName.push_back(_handleName);
+}
+
+int Original::SoundPlay(const char _handleName[])
+{
+	for (int i = 0; i < Original::handleName.size(); i++)
+	{
+		// ハンドル名が一致していたら
+		if (strcmp(Original::handleName[i], _handleName) == 0)
+		{
+			Novice::PlayAudio(Original::handles[i], 0, 0.5f);
+		}
+	}
+
+	return 0;
+}
